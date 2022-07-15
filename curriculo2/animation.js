5e2852bff6fcb22bc80c65d59e99036c2c4b13ba
JoviJustino <81432205+JoviJustino@users.noreply.github.com>
2022-07-15 20:10:23 -0300

    Add files via upload

---
+++ curriculo2/animation.js
@@ -0,0 +1,8 @@
+type =
+  "text/javascript" >
+  document.addEventListener("DOMContentLoaded", function () {
+    window.setTimeout(
+      document.querySelector(".animation").classList.add("animated"),
+      1000
+    );
+  });
