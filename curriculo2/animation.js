a1ad873b202a7dbe7113732530b887bd6418e43b
JoviJustino <81432205+JoviJustino@users.noreply.github.com>
2022-07-15 20:17:04 -0300

    Delete curriculo2 directory

--- curriculo2/animation.js
+++
@@ -1,8 +0,0 @@
-type =
-  "text/javascript" >
-  document.addEventListener("DOMContentLoaded", function () {
-    window.setTimeout(
-      document.querySelector(".animation").classList.add("animated"),
-      1000
-    );
-  });
